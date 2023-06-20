7b5294584d5bab1e87ca534c7fbbd2ff840492fc
fastpooh <119751475+fastpooh@users.noreply.github.com>
2023-06-20 23:55:32 +0900

    little modification on data

--- tongtongShipClient/Assets/Scripts/Des_DuckCtrl.cs
+++ tongtongShipClient/Assets/Scripts/Des_DuckCtrl.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Des_DuckCtrl : MonoBehaviour
 {
-    public float turnSpeed = 0.7f;
-    public int rotateSpeed = 100;
+    public float controlOverBoat = 5f;
+    public int rotateSpeed = 50;
     public float maxSpeed = 7f;
     private CharacterController controller;
     private Rigidbody rb;
@@ -23,13 +23,13 @@ public class Des_DuckCtrl : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(Vector3.up*rotateSpeed*Time.deltaTime*hAxis);
+        transform.Rotate(Vector3.up*rotateSpeed*2*Time.deltaTime*hAxis);
         moveVec = transform.forward;
 
         if(vAxis < -0.9)
             moveVec = -moveVec;
 
-        rb.AddForce(moveVec * turnSpeed * 0.1f);
+        rb.AddForce(moveVec * controlOverBoat * 0.1f);
 
         if (rb.velocity.magnitude > maxSpeed)
         {
