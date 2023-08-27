ec6554cb833fe84a5a54e2b2260a634f008139c7
fastpooh <119751475+fastpooh@users.noreply.github.com>
2023-08-27 12:19:31 +0900

    fix floatingShip, startUI

--- tongtongShipClient/Assets/Scripts/MainButtonManager.cs
+++ tongtongShipClient/Assets/Scripts/MainButtonManager.cs
@@ -7,41 +7,85 @@ using UnityEngine.SceneManagement;
 
 public class MainButtonManager : MonoBehaviour
 {
+    public bool isUIOn;
+    public GameObject blackBackGround;
+    public GameObject multiplayUI;
+    public GameObject statisticsUI;
     public GameObject creditsUI;
 
     private void Start()
     {
         creditsUI.SetActive(false);
+        multiplayUI.SetActive(false);
+        statisticsUI.SetActive(false);
+        blackBackGround.SetActive(false);
+        isUIOn = false;
     }
 
+    // Move to another scene
     public void SinglePlayBtn()
     {
-        SceneManager.LoadScene("SingleScene");
-    }
-
-    public void MultiPlayBtn()
-    {
-        SceneManager.LoadScene("MultiScene");
+        if(!isUIOn)
+            SceneManager.LoadScene("SingleScene");
     }
     
     public void StoreBtn()
     {
-        SceneManager.LoadScene("StoreScene");
+        if(!isUIOn)
+            SceneManager.LoadScene("StoreScene");
+    }
+
+    // Show UI
+    public void MultiPlayBtn()
+    {
+        if(!isUIOn)
+        {
+            multiplayUI.SetActive(true);
+            blackBackGround.SetActive(true);
+            isUIOn = true;
+        }
+        //SceneManager.LoadScene("MultiScene");
+    }
+
+    public void ExitMultiplay()
+    {
+        multiplayUI.SetActive(false);
+        blackBackGround.SetActive(false);
+        isUIOn = false;
     }
 
     public void StatisticsBtn()
     {
-        
+        if(!isUIOn)
+        {
+            statisticsUI.SetActive(true);
+            blackBackGround.SetActive(true);
+            isUIOn = true;
+        }
+    }
+
+    public void ExitStatistice()
+    {
+        statisticsUI.SetActive(false);
+        blackBackGround.SetActive(false);
+        isUIOn = false;
     }
 
     public void CreditsBtn()
     {
-        creditsUI.SetActive(true);
+        if(!isUIOn)
+        {
+            creditsUI.SetActive(true);
+            blackBackGround.SetActive(true);
+            isUIOn = true;
+        }
     }
 
     public void ExitCredits()
     {
         creditsUI.SetActive(false);
+        blackBackGround.SetActive(false);
+        isUIOn = false;
     }
 
     public void EndBtn()
